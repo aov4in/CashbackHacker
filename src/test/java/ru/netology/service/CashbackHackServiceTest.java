fd163a985e24a100fcddcf156b6f39cc547ded1e
Andrei Ovchinnikov <aovchin@gmail.com>
2021-03-27 03:33:14 +0300

    Added branch testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testRemain1100() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1100;
+
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+}
